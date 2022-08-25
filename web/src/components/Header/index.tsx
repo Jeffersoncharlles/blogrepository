fc5e7ee8ec4e853bde7b47b6a127263d212492ec
Jefferson Charlles <user@Jefferson>
2022-08-25 10:15:26 -0300

    chore:start layoutDefault and routes

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,9 +1,10 @@
+import LogoSvg from '../../assets/Logo.svg'
 
 export const Header = () => {
 
     return(
         <header>
-
+            <img src={LogoSvg} alt="Github blog logo e uma seta direita e um traco"/>
         </header>
     );
 }
