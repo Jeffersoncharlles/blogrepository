6211dfaf514e6154931ad65b6e851055dd88bfbe
Jefferson Charlles <contato@jefferdeveloper.com>
2022-09-21 07:20:28 -0300

    chore:formated post

--- web/src/pages/Post/index.tsx
+++ web/src/pages/Post/index.tsx
@@ -1,18 +1,42 @@
-import { useParams } from "react-router-dom";
+import { PostContainer, ArticleContainer, CardContainer, Links, InfoContainer } from './styles'
+import { FaGithub, FaCalendarDay, FaComment, FaExternalLinkAlt } from "react-icons/fa";
+import { Link, useParams } from "react-router-dom";
 import { Markdown } from "../../components/Markdown";
 import { useGitHub } from '../../context/GitHubContext';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 
 
 export const Post = () => {
-    const { getOneArticle } = useGitHub()
+    const { getOneArticle, user } = useGitHub()
     const { slug } = useParams()
     const article = getOneArticle(String(slug))
 
     return (
-        <main>
+        <PostContainer>
+            <CardContainer>
+                <Links>
+                    <Link to="/">
+                        voltar
+                    </Link>
+                    <a target="_blank" href={article.url}>
+                        ver no github
+                        <FaExternalLinkAlt />
+                    </a>
+                </Links>
+                <h1>{article?.title}</h1>
+                <InfoContainer>
+                    <span><FaGithub />{user.login}</span>
+                    <span><FaCalendarDay /> {article.date}</span>
+                    <span><FaComment />5 comentários</span>
 
-            <Markdown markdown={article?.content} />
-        </main>
+                </InfoContainer>
+            </CardContainer>
+
+            <ArticleContainer>
+                <Markdown markdown={article?.content} />
+                {/* <ReactMarkdown children={article.content} /> */}
+            </ArticleContainer>
+        </PostContainer>
     );
 }
---
+++ web/src/pages/Post/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+export const PostContainer = styled.main`
+    width: min(1440px, 100%);
+    margin: 0 auto;
+    margin-bottom: 48px;
+
+
+`;
+
+export const ArticleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 40px 32px;
+    gap: 24px;
+`;
+export const CardContainer = styled.div`
+    margin-top: -64px;
+    background: ${({ theme }) => theme.colors.baseProfile};
+    box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+    padding: 32px;
+
+    h1{
+        font-size: ${({ theme }) => theme.fonts[24]};
+        line-height: 1.3;
+
+    }
+`;
+
+
+export const Links = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    color: ${({ theme }) => theme.colors.blue};
+
+    a{
+        font-size: ${({ theme }) => theme.fonts[14]};
+        color: ${({ theme }) => theme.colors.blue};
+        font-weight: 700;
+        letter-spacing: 1.5px;
+        text-transform: uppercase;
+        align-self: center;
+        justify-self: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 4px;
+
+        &:hover{
+            filter: brightness(.8);
+        }
+
+    }
+`;
+
+export const InfoContainer = styled.div`
+    color: ${({ theme }) => theme.colors.baseSpan};
+    margin-top: 1rem;
+    display: flex;
+    gap: 32px;
+
+    span{
+        display: flex;
+        align-items: center;
+
+        svg{
+            margin-right: 8px;
+        }
+    }
+`;
